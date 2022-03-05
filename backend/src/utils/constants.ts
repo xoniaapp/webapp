888ec10bb3e3556ef202ffe0eaa9ca77efdf90f8
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-03-05 20:16:26 +0600

    no longer a monorepo

--- backend/src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const COOKIE_NAME = 'xa';
-export const FORGET_PASSWORD_PREFIX = 'forget-password:';
-export const INVITE_LINK_PREFIX = 'inviteLink:';
-export const PRODUCTION = process.env.NODE_ENV === 'production';
