3204b13f5ed7f8aa5ed68da68ccda6f4abb7fdae
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-21 23:03:47 +0600

    fix: bug

--- src/components/common/NotificationIcon.tsx
+++ src/components/common/NotificationIcon.tsx
@@ -38,7 +38,7 @@ export const PingIcon: React.FC<NotificationIconProps> = ({ count }) => (
     align="center"
     ml={2}
   >
-    {/* TODO: Once a notification arrives play an sound. */}
+    {/*TODO: Once a notification arrives play a sound.*/}
     <Text fontSize="11px" color="white">
       {count}
     </Text>
