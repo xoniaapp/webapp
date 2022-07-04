f7958b5ddb3ffeda4b87147dd251351e1275e894
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-07-04 22:59:15 +0600

    bug: theme failed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from "react-dom/client"
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
+import customTheme from "./lib/utils/theme"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
 root.render(
   <>
-    <ChakraProvider theme={theme}>
+    <ColorModeScript />
+    <ChakraProvider theme={customTheme}>
       <App />
     </ChakraProvider>
   </>,
