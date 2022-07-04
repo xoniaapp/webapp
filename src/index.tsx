7d450c1f2770d878057139deb00c46f23e3126a3
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-07-04 22:55:09 +0600

    feat: test v1

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
 import { createRoot } from "react-dom/client"
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+import { ChakraProvider, theme } from '@chakra-ui/react'
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
-import customTheme from "./lib/utils/theme"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
 root.render(
   <>
-    <ColorModeScript />
-    <ChakraProvider theme={customTheme}>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </>,
